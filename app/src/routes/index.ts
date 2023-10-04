59e7058e215b0d398c25e3e85a14881f2a455083
David Marques <davidmarques99@hotmail.com>
2023-10-04 18:07:28 -0300

    Criado rota GET para filtrar um produto pelo ID

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -1,12 +1,14 @@
 // Extraindo apenas o módulo Router da biblioteca express
 import { Router } from "express";
 import productsRouterGetAll from "./products-get-all.routes";
+import productsRouterGetId from "./products-get-id.routes";
 
 
 // Criando uma instancia de Router dentro da constante routes
 const routes = Router();
 
 routes.use('/products', productsRouterGetAll)
+routes.use('/products/search', productsRouterGetId)
 
 
 export default routes;
