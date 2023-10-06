4f8b7ffa964c41013ba23bbc77e063df0c267679
davidmarquescoder <davidmarques99@hotmail.com>
2023-10-05 21:49:16 -0300

    Tratando os dados na rota de update

--- app/src/routes/products-put.routes.ts
+++ app/src/routes/products-put.routes.ts
@@ -9,6 +9,35 @@ productsRouterPut.put('/:id', async (request, response) => {
 
     const {name, description, price, stock} = request.body;
 
+    // Verificação de preenchimento de nome do produto
+    if(name == null){
+        return response.json({message: "É obrigatório informar um nome para o produto."})
+    }
+
+    // Tratamento do preço do produto
+    if(price == null){
+        return response.json({message: "É obrigatório informar um preço para o produto."})
+    }
+    else if(price <= 0){
+        return response.json({message: "O preço do produto deve ser um valor positivo."})
+    }
+
+    // Tratamento do estoque
+    if(stock == null){
+        return response.json({message: "É obrigatório informar a quantidade em estoque."})
+    }
+    else if(stock <= 0){
+        return response.json({message: "O estoque do produto deve ter um valor positivo."})
+    }
+    else if(!Number.isInteger(stock)){
+        return response.json({message: "O valor inserido no campo do estoque deve ser um número inteiro."})
+    }
+
+    // Tratamento do limite de caracteres na descrição do produto
+    if(description.length > 500){
+        return response.json({message: "A descrição não pode ter mais que 500 caracteres."});
+    };
+
     const product = {
         name,
         description,
