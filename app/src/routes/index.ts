ad424c7f8018f39a00a30c86a859cb9379a513c2
David Marques <davidmarques99@hotmail.com>
2023-10-28 12:36:38 -0300

    Liberação de cors para local host porta 3000

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -4,15 +4,16 @@ import productsRouterGetId from "./products-get-id.routes";
 import productsRouterPost from "./products-post.routes";
 import productsRouterPut from "./products-put.routes";
 import productsRouterDelete from "./products-delete.routes";
+import cors from "cors";
 
 
 const routes = Router();
 
-routes.use('/products', productsRouterGetAll)
-routes.use('/products/search', productsRouterGetId)
-routes.use('/products', productsRouterPost)
-routes.use('/products', productsRouterPut)
-routes.use('/products', productsRouterDelete)
+routes.use('/products', cors(), productsRouterGetAll)
+routes.use('/products/search', cors(), productsRouterGetId)
+routes.use('/products', cors(),productsRouterPost)
+routes.use('/products', cors(),productsRouterPut)
+routes.use('/products', cors(),productsRouterDelete)
 
 
 export default routes;
--- app/src/server.ts
+++ app/src/server.ts
@@ -1,8 +1,15 @@
 import express, { request, response } from "express";
 import routes from "./routes";
+import cors from "cors";
 
 
 const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    app.use(cors());
+    next();
+});
 
 app.use(express.json());
 app.use(routes)
