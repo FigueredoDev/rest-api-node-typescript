f4f3211dd54c45001ad1eb2182639b36df2afb91
JhonataFigueredo <jhowfigueredo2@gmail.com>
2023-10-30 11:00:04 -0300

    refactor: removed unused types from express

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,19 +1,18 @@
-import express, { request, response } from "express";
+import express from "express";
 import routes from "./routes";
 import cors from "cors";
-
 
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    app.use(cors());
-    next();
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  app.use(cors());
+  next();
 });
 
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
 app.listen(3333, () => {
-    console.log("📌 Beckend started on port 3333 👌")
-})
+  console.log("📌 Server started on port 3333 👌");
+});
